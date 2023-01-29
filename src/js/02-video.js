bd77e852cf5e62fd4decf78fc309866d21c73640
Oleksandr Tytyk <aleks.tit3@gmail.com>
2023-01-29 13:58:50 +0200

    ....

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import throttle from "lodash.throttle";
+import player from "@vimeo/player";
 
+const iframe = document.querySelector('iframe');
+console.log('iframe', iframe);
+const player = new Vimeo.Player(iframe);
+console.log('player', player);
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
